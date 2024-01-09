257757719e8151cbe553ebf4f6ebc5ee0b17da99
ViktoriaSharpershape <viktoriia.shevchenko@sharpershape.com>
2024-01-09 22:51:01 +0200

    additional math

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const words = "m"
+const words = "Viktoria's Animation"
+
+const ANIMATION_DURATION = 4000
 
 const characters = words.split("").forEach((char, i) => {
     function createElement(offset) {
         const div = document.createElement("div")
         div.innerText = char
         div.classList.add("character")
+        div.style.animationDelay = `-${i * (ANIMATION_DURATION / 16) - offset}ms`
 
         return div
     }
